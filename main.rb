7a550551aa3cb8513c78acf0022af17c9d9b5aa9
Julian Nicholls <juliannicholls29@gmail.com>
2013-09-19 12:58:30 +0100

    In a fix. Just added flash functionality and its broken, I think

--- main.rb
+++ main.rb
@@ -3,9 +3,20 @@ require 'sinatra/reloader' if development?
 require 'data_mapper'
 require 'sass'
 require 'slim'
+require 'builder'
+require 'sinatra/flash'
+# require 'sinatra/redirect_with_flash'
+
+
+SITE_TITLE       = "Turtle Recall2"
+SITE_DESCRIPTION = "'cause you need to remember when you're that old"
+
+use Rack::Session::Cookie, 
+  :expire_after => 60,     # One minute should cover it.
+  :secret => 'JGNTR2'      # Finally, an actual secret
+    
 
 DataMapper::setup( :default, "sqlite3://#{Dir.pwd}/recall.db" )
-
 
 class Note
   include DataMapper::Resource
@@ -23,68 +34,82 @@ end
 DataMapper.finalize.auto_upgrade!
 
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
+
 # Handlers
-
-get( '/styles/main.css' ) { scss :styles }
-
 
 get '/' do      # Home page
   @notes = Note.all :order => :id.desc   # This will become :due_at.asc
   @title = 'All Shells'
   
+  if @notes.empty?
+    puts "Setting flash[:error]"
+    flash[:error] = "No shells found. Add the first one below."
+  end
+  
   slim :home
 end
 
 
-post '/' do     # Add a shell
-  puts "Adding Shell"
+get '/rss.xml' do   # Give an RSS feed
+  @notes = Note.all :order => :id.desc   # This will become :due_at.asc
+  builder :rss
+end
+
   
+post '/' do     # Add a shell
   n = Note.new
   n.content = params[:content]
   n.created_at = Time.now
   n.updated_at = Time.now
-# n.due_at = params[:due_at]
+#  n.due_at = params[:due_at]
   n.save
-  redirect to('/')
+  redirect '/'
 end
 
 
-get '/:id' do
+get '/:id' do   # Start edit
   @note  = Note.get params[:id]
-  @title = "Edit Note ##{params[:id]}"
+  @title = "Edit Shell ##{params[:id]}"
   slim :edit
 end
 
 
-__END__
+put '/:id' do   # Update
+  n = Note.get params[:id]
+  n.content  = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+#  n.due_at = params[:due_at]
+  n.save
+  redirect '/'
+end
 
-@@styles
-$done: #ddffdd;
 
-form#add {
-  margin-bottom: 20px;
-  
-  textarea {
-    width: 850px;
-    margin-right: 20px;
-  }
-}
+get '/:id/delete' do  # Start delete
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of Shell ##{params[:id]}"
+  slim :delete
+end
 
-button {
-  a       { color: white; }
-  a:hover { color: white; text-decoration: none }
-}
 
-article {
-  border: 1px solid #e5e5e5;
-  padding: 5px;
-  margin-bottom: 2px;
-}
+delete '/:id' do      # Delete
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
 
-article.complete {
-    background: $done;
-}
 
-div#footer {
-  margin-top: 20px;
-}
+get '/:id/complete' do  # Mark complete
+  n = Note.get [:id]
+  n.complete = n.complete ? 0 : 1 # Flip it
+  n.completed_at = n.complete ? Time.now : nil
+  n.updated_at = Time.now
+  n.save
+  redirect to('/')
+end
+
